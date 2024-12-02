64dbf53cce0bd93fa410928510457252cb3e87de
Emmanuel Bizieau <emmanuel.bizieau@orange.com>
2024-12-02 22:13:01 +0100

    evaluating: literal strings and numbers

--- app/evaluating.py
+++ app/evaluating.py
@@ -3,6 +3,10 @@ from syntax import Literal
 
 def evaluate(node):
     match node:
+        case Literal(float()):
+            """Print the number 'with the minimum number of decimal places without losing precision'."""
+            return str(node.value).rstrip("0").rstrip(".")
+
         case Literal():
             return repr(node)
         
--- test/integration/test_int_evaluating.py
+++ test/integration/test_int_evaluating.py
@@ -10,3 +10,12 @@ def test_evaluate_literals(run_lox):
 
     _, output, _ = run_lox(command="evaluate", lox_source="nil")
     assert output == "nil"
+
+    _, output, _ = run_lox(command="evaluate", lox_source='"hello world!"')
+    assert output == "hello world!"
+
+    _, output, _ = run_lox(command="evaluate", lox_source="10")
+    assert output == "10"
+
+    _, output, _ = run_lox(command="evaluate", lox_source="10.40")
+    assert output == "10.4"
